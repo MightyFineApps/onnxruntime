3b2c9d0f7bf5bb7a8f06283d951c89c3947fb28d
Dan Halliday <dan@halliday.work>
2023-02-28 14:31:35 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -23,12 +23,12 @@ let package = Package(
                 ], linkerSettings: [
                     .unsafeFlags(["-ObjC"]),
                 ]),
-        
+
         // to generate checksum use `shasum -a 256 path/tp/my/zip` or `swift package compute-checksum path/tp/my/zip`
         .binaryTarget(name: "onnxruntime",
-                      url: "https://onnxruntimepackages.z14.web.core.windows.net/pod-archive-onnxruntime-c-1.13.1.zip",
-                      checksum: "f83c847421a6198d060af5e8535314cfe540a208552db7870b838ebc90cdaa3f"),
-        
+                      url: "https://onnxruntimepackages.z14.web.core.windows.net/pod-archive-onnxruntime-c-1.14.0.zip",
+                      checksum: "c89cd106ff02eb3892243acd7c4f2bd8e68c2c94f2751b5e35f98722e10c042b"),
+
         .testTarget(name: "onnxTests",
                     dependencies: ["OnnxWrapper"],
                     path: "swift/onnxTests"),
